0cd468a8e96ac640f2fa18e1a6c6848ca093aeea
Alexandre Esteves <alexandreestevesfclaro@gmail.com>
2024-05-03 18:29:11 -0300

    Start do projeto

---
+++ src/main/java/dev/alxndrestvs/efdreinf/EfdReinfApplication.java
@@ -0,0 +1,13 @@
+package dev.alxndrestvs.efdreinf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfdReinfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfdReinfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alxndrestvs/efdreinf/EfdReinfApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alxndrestvs.efdreinf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfdReinfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
